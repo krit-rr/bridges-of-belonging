74840849350a41cd7d0ad35216086cfe41f69fc2
kritika-rr <kritikaravichander@gmail.com>
2026-05-12 01:18:47 -0400

    ffe

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Submit from './form'
 import Graph from './graph'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/bridges-of-belonging">
+    <HashRouter basename="/bridges-of-belonging">
       <Routes>
         <Route path="/" element={<Submit />} />
         <Route path="/form" element={<Submit />} />
         <Route path="/graph" element={<Graph />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
